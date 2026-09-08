2ff04947e29d65371958584b4f51577142cdf76c
zenith17-virgo <17zenithvirgo@gmail.com>
2026-09-08 19:42:43 +0530

    Wifi config

--- IoT_project.ino
+++ IoT_project.ino
@@ -1,7 +1,19 @@
+#include <WiFi.h>
+ const char* ssid = "CAMPUS CONNECT CUT";
+ const char* password = "075289";
 void setup() {
-  Serial.begin(115200);
-  Serial.begin("System Initializing...");
+    Serial.begin(115200);
+    delay(1000);
+
+  Serial.println("System Initializing");
+  Wifi.begin(ssid, password);
+
+  while (Wifi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 }
+  Serial.print("Connected successfully");
 
 void loop() {
   
