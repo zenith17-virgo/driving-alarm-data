042c4c53eff2f2cb5b5b41009152b22ac507dc94
zenith17-virgo <17zenithvirgo@gmail.com>
2026-09-08 20:01:46 +0530

    ad8232-ecg

--- IoT_project.ino
+++ IoT_project.ino
@@ -8,6 +8,9 @@ void setup() {
   Serial.println("System Initializing");
   Wifi.begin(ssid, password);
 
+  pinMode(32, INPUT); //LO-plus
+  pinMode(33, INPUT); //LO-minus
+  pinMode(34, INPUT); //ecg voltage
   while (Wifi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -15,6 +18,20 @@ void setup() {
 }
   Serial.print("Connected successfully");
 
+  int ecgread() {
+    if (digitalRead(32) == 1 || digitalRead(33) == 1) {
+      Serial.println("Leads not connected properly");
+      return 0;
+    }
+    else {
+      int value = analogRead(34);
+      return value;
+    }  
+  }
 void loop() {
-  
+  int signal = ecgread();
+  Serial.print("Signal is: ");
+  Serial.println(signal);
+
+  delay(20);
 }
