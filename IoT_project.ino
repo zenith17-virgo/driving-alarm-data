4968882c73ff3a33b9d28c9103129cc736c50d77
zenith17-virgo <17zenithvirgo@gmail.com>
2026-09-10 22:43:24 +0530

    setup

--- IoT_project.ino
+++ IoT_project.ino
@@ -1,42 +1,21 @@
-#include <WiFi.h>
-#include <AdafruitIO_Wifi.h>
+#include <Wire.h>
+#include <MAX30105.h>
+#include <heartrate.h>
 
-const char* ssid = "CAMPUS CONNECT CUT";
-const char* password = "075289";
+MAX30105 particleSensor;
 
-#define IO_USERNAME  "z3n1thviRgo"
-#define IO_KEY       "aio_LLwC851dbM4WeORHogPPGWWkmJCz"
+long lastbeat = 0;
+float bpm = 0;
+int avgbeat = 0;
 
-#define ad8232_out 34
-#define ad8232_lop 32
-#define ad8232_lom 33
-
-AdafruitIO_Wifi io(IO_USERNAME, IO_KEY, ssid, password);
-AdafruitIO_Feed *ecgFeed = io.feed("ecg");
-//other sensors will add later
-unsigned long lastpublishtime = 0;
-const unsigned long interval = 10000;
 void setup() {
-    Serial.begin(115200);
-    delay(1000);
-  //write pinmodes for all sensors later here
-  Serial.println("Connecting to IO...");
-  io.connect();
+  Serial.begin(115200);
 
-  while (io.status() < AIO_CONNECTED) {
-    Serial.print(".");
-    delay(500);
+  while (!Serial);
+
+  Wire.begin(21, 22);
+
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST))  {
+    
   }
-
-  Serial.println(io.statusText());
-}
-  
-void loop() {
-  io.run(); 
-
-  if (millis() - lastpublishtime >= interval) {
-    lastpublishtime = millis();
-  }
-
-  //will configure for all sensors later on
 }
