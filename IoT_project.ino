202c837cb97217c86f9f90f84a823a2a165f7262
zenith17-virgo <17zenithvirgo@gmail.com>
2026-09-12 13:02:32 +0530

    spo2 sensor

--- IoT_project.ino
+++ IoT_project.ino
@@ -1,21 +1,42 @@
 #include <Wire.h>
 #include <MAX30105.h>
-#include <heartrate.h>
+#include <spo2_algorithm.h>
 
 MAX30105 particleSensor;
 
-long lastbeat = 0;
-float bpm = 0;
-int avgbeat = 0;
+uint32_t irbuffer[100];
+uint32_t redbuffer[100];
+
+int32_t spo2;
+int8_t validspo2;
+int32_t heartrate;
+int8_t validheartrate;
 
 void setup() {
   Serial.begin(115200);
-
-  while (!Serial);
-
   Wire.begin(21, 22);
 
   if (!particleSensor.begin(Wire, I2C_SPEED_FAST))  {
-    
+    Sertial.println("Sensor not found");
+    while (1 == 1);
+  }
+  particleSensor.setup();
+  Serial.println("Initialized");
+}
+
+void loop () {
+  for (byte i = 0; i < 100; i++) {
+    while (!particleSensor.available())
+      particleSensor.check();
+    redbuffer[i] = particleSensor.getRed();
+    irbuffer[i] = particleSensor.getIR();
+    particleSensor.nextSample();
+    }
+
+  maxim_spo2_accuracy(irbuffer, 100, redbuffer, &spo2, &validspo2, &heartrate, &validheartrate);
+
+  if (validheartrate == 1 && validspo2 == 1) {
+    Serial.print("Spo2: ");
+    Serial.print(spo2);
   }
 }
